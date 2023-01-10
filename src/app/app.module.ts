bf154361dd8b58f78d3d1f28740b0607a8bae5b0
Iyan Theekshana <iyan.dev123@gmail.com>
2023-01-10 13:04:30 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SuccessAlertComponent } from './successAlert/successAlert.component';
+import { WarningAlertComponent } from './warningAlert/warningAlert.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, SuccessAlertComponent, WarningAlertComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
